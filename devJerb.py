8c3d3d1cf7a536ba1b176caa6920350e97833f4a
devJerb <gutierrez.jeromebryant@gmail.com>
2022-06-23 14:35:42 +0800

    first commit

--- devJerb.py
+++ devJerb.py
@@ -4,10 +4,11 @@ from rich.console import Console
 from rich.columns import Columns
 from rich import box
 
-console = Console(record=True, width=100)
+console = Console(record=True, width=100, height="100")
 
 directory = Tree("[link=https://www.facebook.com/gutierrez.jerby/]Jerome Gutierrez", guide_style="bold white")
 
+# tools for web dev
 path = directory.add("💻 Web Development", guide_style="bold blue")
 path.add("JS, HTML, & CSS")
 framework = path.add("React JS")
@@ -15,6 +16,7 @@ framework.add("Bootstrap")
 framework.add("Chakra")
 framework.add("Three")
 
+# other languages known
 other = directory.add("👀 Other Languages", guide_style="bold yellow")
 other.add("Python")
 other.add("Java")
@@ -22,20 +24,24 @@ other.add("Dart")
 other.add("C++")
 other.add("SQL")
 
-
-sites = directory.add("⚔️ Competitive", guide_style="bold red")
+# competitive site/s
+sites = directory.add("⚔️  Competitive", guide_style="bold red")
 sites.add("[link=https://www.codewars.com/dashboard]Codewars")
 
+# bio
 description = """
     Hi!👋, I'm an enthusiast of technology and its advancements; currently an undergraduate studying BS - CSE. I love gaming too!
 
     My forte is web development wherein I focus on [link=https://www.javascript.com/]Javascript[/], I do side projects with [link=https://www.python.org/]Python[/] and [link=https://www.java.com/en/]Java[/].
+
+    I love listening and creating music so catch me on [link=https://soundcloud.com/discover]Soundcloud[/]! 🎶 
 """
 
+# output
 panel = Panel.fit(description, border_style="red", box=box.DOUBLE, title="Development Phase", width=60)
 console.print(Columns([panel, directory]))
-FORMAT = """\
-<pre style="font-family:Menlo,'DejaVu Sans Mono',consolas,'Courier New',monospace">{code}</pre>
+FORMAT = """
+<pre style="font-family: 'Montserrat', sans-serif">{code}</pre>
 """
 
 console.save_html("README.md", inline_styles=True, code_format=FORMAT)
