6487bd4b4c869e5e67078f02a0987b837d2bec7d
devJerb <gutierrez.jeromebryant@gmail.com>
2022-07-06 22:39:23 +0800

    first commit

--- devJerb.py
+++ devJerb.py
@@ -9,7 +9,7 @@ console = Console(record=True, width=150, height=100)
 directory = Tree("[link=https://www.facebook.com/gutierrez.jerby/]Jerome Gutierrez", guide_style="bold white")
 
 # tools for web dev
-path = directory.add("💻 Web Development", guide_style="bold blue")
+path = directory.add("💻 Programming", guide_style="bold blue")
 path.add("JavaScript")
 path.add("React")
 path.add("Java")
@@ -17,14 +17,13 @@ path.add("Python")
 
 # bio
 description = """
-    Hi!👋, I'm an enthusiast of technology.
+    Hi!👋, I like technology!
 
-    I am currently an undergraduate studying BS - CSE.
+    I am an undergraduate studying BS - CSE.
 
     I like making websites! I focus on [link=https://www.javascript.com/]Javascript[/].
-    I do side projects with [link=https://www.python.org/]Python[/] and [link=https://www.java.com/en/]Java[/].
 
-    I love music so catch me on [link=https://soundcloud.com/discover]Soundcloud[/]! 🎶 
+    I love music check my [link=https://soundcloud.com/discover]Soundcloud[/]! 🎶 
 """
 
 # output
